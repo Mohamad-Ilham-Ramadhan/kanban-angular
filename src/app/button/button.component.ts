e616810a15be405042040fd0c136df4068abab0c
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-09-18 10:27:12 +0700

    component, style: ButtonComponent: add @Input type, size, color, classes

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+  @Input('type') type: 'button' | 'submit' = 'button';
+  @Input('size') size: 'medium' | 'small' = 'medium';
+  @Input('color') color: 'primary' | 'white' = 'primary';
+  @Input('classes') classes: string = '';
+  
 }
